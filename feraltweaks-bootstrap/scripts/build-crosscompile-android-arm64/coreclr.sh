8bc12e7bfa7927524bb6442c383555e072c2181b
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2023-10-04 10:33:08 +0200

    More work on the build workflow (sorry for the many commits, i cannot compile this locally rn)

--- feraltweaks-bootstrap/scripts/build-crosscompile-android-arm64/coreclr.sh
+++ feraltweaks-bootstrap/scripts/build-crosscompile-android-arm64/coreclr.sh
@@ -17,7 +17,7 @@ coreclrbranch="release/6.0"
 # Download CoreCLR
 echo Cloning CoreCLR...
 mkdir coreclr
-git clone "$coreclrrepo" --branch "$coreclrbranch" coreclr/repo
+git clone "$coreclrrepo" coreclr/repo
 
 # Build CoreCLR
 echo Building CoreCLR...
@@ -26,6 +26,10 @@ cd coreclr/repo
 # Prepare
 echo Preparing crosscompile...
 bash eng/common/cross/build-android-rootfs.sh || exit 1
+echo Switching branch...
+git checkout "$coreclrbranch" || exit 1
+git pull
 
 # Build
 echo Compiling...
+ROOTFS_DIR=$(realpath ./.tools/android-rootfs/android-ndk-*/sysroot) ./build.sh clr+libs --cross --arch arm64 || exit 1
