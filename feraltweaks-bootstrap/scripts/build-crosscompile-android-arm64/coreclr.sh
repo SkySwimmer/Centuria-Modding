eba70f46ad996333c6380e75e59aee3b43a887c3
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2024-06-20 09:59:26 +0200

    Attempt further work on android (7)

--- feraltweaks-bootstrap/scripts/build-crosscompile-android-arm64/coreclr.sh
+++ feraltweaks-bootstrap/scripts/build-crosscompile-android-arm64/coreclr.sh
@@ -11,35 +11,32 @@ cd build/work
 
 
 # Properties
-coreclrrepo="https://github.com/dotnet/runtime"
-coreclrbranch="release/8.0"
+monorepo="https://github.com/dotnet/runtime"
+monobranch="release/8.0"
 
 # Download .NET runtime
 echo Cloning .NET runtime...
 mkdir runtime
-git clone "$coreclrrepo" runtime/repo
+git clone "$monorepo" runtime/repo
 
-# Build CoreCLR
+# Build Mono
 echo Building .NET runtime...
 cd runtime/repo
 
 # Prepare
 echo Preparing crosscompile...
-cd eng/common
-bash cross/build-android-rootfs.sh || exit 1
+bash eng/common/cross/build-android-rootfs.sh || exit 1
 echo Switching branch...
-git checkout "$coreclrbranch" || exit 1
+git checkout "$monobranch" || exit 1
 git pull
 
 # Build
 echo Compiling...
-ROOTFS_DIR=$(realpath ../../.tools/android-rootfs/android-ndk-*/sysroot) ./build.sh --cross --arch arm64 --subsetCategory coreclr || exit 1
-ROOTFS_DIR=$(realpath ../../.tools/android-rootfs/android-ndk-*/sysroot) ./build.sh --cross --arch arm64 --subsetCategory libraries || exit 1
-cd ../..
+ROOTFS_DIR=$(realpath ./.tools/android-rootfs/android-ndk-*/sysroot) ./build.sh mono+libs --cross --arch arm64 -os android -c release /p:RunAOTCompilation=false /p:MonoForceInterpreter=true || exit 1
 
 # Copy
 echo Copying files...
-mkdir ../../coreclrlib
-cp -rfv artifacts/obj/mono/*nux.arm64.*/out/include/. ../../coreclrlib/include
-cp -rfv artifacts/bin/microsoft.netcore.app.runtime.*/*/runtimes/*/native/. ../../coreclrlib/lib
-cp -rfv artifacts/bin/microsoft.netcore.app.runtime.*/*/runtimes/*/lib/*/. ../../coreclrlib/lib
+mkdir ../../monolib
+cp -rfv artifacts/obj/mono/*nux.arm64.*/out/include/. ../../monolib/include
+cp -rfv artifacts/bin/microsoft.netcore.app.runtime.*/*/runtimes/*/native/. ../../monolib/lib
+cp -rfv artifacts/bin/microsoft.netcore.app.runtime.*/*/runtimes/*/lib/*/. ../../monolib/lib
