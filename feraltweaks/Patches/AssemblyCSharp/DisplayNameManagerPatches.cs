eacb565bc24971322218583923f54e156bb2a2ee
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2026-01-16 21:30:41 +0100

    Backport improved display name fetcher logic

--- feraltweaks/Patches/AssemblyCSharp/DisplayNameManagerPatches.cs
+++ feraltweaks/Patches/AssemblyCSharp/DisplayNameManagerPatches.cs
@@ -10,54 +10,132 @@ using Il2CppSystem.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using System.Reflection;
 
 namespace feraltweaks.Patches.AssemblyCSharp
 {
     public class DisplayNameManagerPatches
     {
-        private static List<string> inProgressDisplayNames = new List<string>();
-        private static List<string> fetchDisplayNames = new List<string>();
+        private class DisplayNameState
+        {
+            private object _lock = new object();
+
+            private Exception ex;
+            public bool complete;
+            public string result;
+
+            public void Call(string result)
+            {
+                lock (_lock)
+                {
+                    this.result = result;
+                    complete = true;
+                }
+
+                // Release
+                lock (_lock)
+                {
+                    Monitor.PulseAll(_lock);
+                }
+            }
+
+            public void Error(Exception ex)
+            {
+                lock (_lock)
+                {
+                    this.ex = ex;
+                    result = null;
+                    complete = true;
+                }
+
+                // Release
+                lock (_lock)
+                {
+                    Monitor.PulseAll(_lock);
+                }
+            }
+
+            public string Await()
+            {
+                lock (_lock)
+                {
+                    // Check exception
+                    if (ex != null)
+                        throw new TargetInvocationException("Target function has thrown an exception", ex); // Throw exception
+
+                    // Check completed
+                    if (complete)
+                        return result;
+
+                    // Wait
+                    while (!complete)
+                        Monitor.Wait(_lock);
+                }
+
+                // Check exception
+                if (ex != null)
+                    throw new TargetInvocationException("Target function has thrown an exception", ex); // Throw exception
+
+                // Check completed
+                if (complete)
+                    return result;
+                return null;
+            }
+        }
+
+        private static List<string> displayNamesToFetch = new List<string>();
+        private static Dictionary<string, DisplayNameState> inProgressDisplayNames = new Dictionary<string, DisplayNameState>();
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(UserManager), "GetDisplayNameBatched")]
         public static bool GetDisplayNameBatched(UserManager __instance, string inUUID, ref Task<string> __result)
         {
+            // Already present, return
             if (__instance._users._usersByUUID.ContainsKey(inUUID))
+            {
                 __result = Task.Run<string>(new Func<string>(() =>
                 {
                     return __instance._users._usersByUUID[inUUID].Name;
                 }));
+                return false;
+            }
+
+            // Request
             __result = Task.Run<string>(new Func<string>(() =>
             {
                 // Check in progress
-                lock (fetchDisplayNames)
+                DisplayNameState state = null;
+                lock (inProgressDisplayNames)
                 {
-                    if (!inProgressDisplayNames.Contains(inUUID))
+                    lock (displayNamesToFetch)
                     {
-                        if (!fetchDisplayNames.Contains(inUUID))
+                        // Check if present
+                        if (!inProgressDisplayNames.ContainsKey(inUUID))
                         {
-                            fetchDisplayNames.Add(inUUID);
+                            // Check if present
+                            if (__instance._users._usersByUUID.ContainsKey(inUUID))
+                                return __instance._users._usersByUUID[inUUID].Name;
+
+                            // Add if needed
+                            if (!displayNamesToFetch.Contains(inUUID))
+                            {
+                                // Add display name
+                                displayNamesToFetch.Add(inUUID);
+
+                                // Create promise
+                                inProgressDisplayNames[inUUID] = new DisplayNameState();
+                            }
                         }
+
+                        // Get promise
+                        state = inProgressDisplayNames[inUUID];
+                        if (state.complete)
+                            return state.result;
                     }
                 }
 
-                // Wait for response
-                long start = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                while (!__instance._users._usersByUUID.ContainsKey(inUUID))
-                {
-                    // Check
-                    if (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - start >= 30000)
-                        return null;
-
-                    // Check in progress
-                    if (!inProgressDisplayNames.Contains(inUUID) && !fetchDisplayNames.Contains(inUUID))
-                        return null; // Failure
-
-                    // Wait
-                    Thread.Sleep(10);
-                }
-
-                return __instance._users._usersByUUID[inUUID].Name;
+                // Await promise
+                return state.Await();
             }));
             return false;
         }
@@ -66,69 +144,106 @@ namespace feraltweaks.Patches.AssemblyCSharp
         [HarmonyPatch(typeof(UserManager), "LateUpdate")]
         public static void LateUpdate(UserManager __instance)
         {
+            string[] namesToFetch = new string[0];
             lock (inProgressDisplayNames)
             {
-                lock (fetchDisplayNames)
+                lock (displayNamesToFetch)
                 {
                     // Get names to fetch
-                    string[] namesToFetch = fetchDisplayNames.ToArray();
+                    namesToFetch = displayNamesToFetch.ToArray();
                     if (namesToFetch.Count() != 0)
                     {
-                        fetchDisplayNames.Clear();
-                        inProgressDisplayNames.AddRange(namesToFetch);
+                        // Clear list
+                        displayNamesToFetch.Clear();
 
-                        // Fetch
-                        Il2CppSystem.Collections.Generic.List<string> ls = new Il2CppSystem.Collections.Generic.List<string>();
-                        foreach (string name in namesToFetch)
-                            ls.Add(name);
-                        Task<WWWResponse<IdentityDisplayNamesResponse>> task = IdentityService.GetDisplayNames(ls, NetworkManager.JWT);
-                        TaskAwaiter<WWWResponse<IdentityDisplayNamesResponse>> awaiter = task.GetAwaiter();
-                        feraltweaks.FeralTweaks.ScheduleDelayedActionForUnity(() =>
+                        // Create promises
+                        foreach (string id in namesToFetch)
                         {
-                            // Wait for completion
-                            if (!awaiter.IsCompleted)
-                                return false;
-
-                            // Get result
-                            WWWResponse<IdentityDisplayNamesResponse> res = awaiter.GetResult();
-
-                            // Remove found display names, re-queue non-found
-                            if (res.IsSuccess)
-                            {
-                                // Add successful name fetches
-                                foreach (IdentityDisplayNamesResponse.Identity id in res.value.found)
-                                {
-                                    if (!__instance._users._usersByUUID.ContainsKey(id.uuid))
-                                    {
-                                        // Add
-                                        UserInfo uInfo = new UserInfo(id.uuid, id.display_name);
-                                        __instance._users.Add(uInfo);
-                                        __instance._users._usersByUUID[id.uuid] = uInfo;
-                                    }
-                                }
-
-                                // Done
-                                lock (inProgressDisplayNames)
-                                {
-                                    foreach (string name in namesToFetch)
-                                        inProgressDisplayNames.Remove(name);
-                                }
-                            }
-                            else
-                            {
-                                // Failure
-                                lock (inProgressDisplayNames)
-                                {
-                                    foreach (string name in namesToFetch)
-                                        inProgressDisplayNames.Remove(name);
-                                }
-                            }
-
-                            // Done
-                            return true;
-                        });
+                            if (!inProgressDisplayNames.ContainsKey(id))
+                                inProgressDisplayNames[id] = new DisplayNameState();
+                        }
                     }
                 }
+            }
+
+            if (namesToFetch.Length != 0)
+            {
+                // Fetch display names
+                // First, create the list
+                Il2CppSystem.Collections.Generic.List<string> ls = new Il2CppSystem.Collections.Generic.List<string>();
+                foreach (string name in namesToFetch)
+                    ls.Add(name);
+
+                // Call the server
+                Task<WWWResponse<IdentityDisplayNamesResponse>> tsk = IdentityService.GetDisplayNames(ls, NetworkManager.JWT);
+                FeralTweaks.ScheduleDelayedAction(() =>
+                {
+                    // Wait
+                    if (!tsk.IsCompleted)
+                        return false;
+
+                    // Get result
+                    WWWResponse<IdentityDisplayNamesResponse> res = tsk.GetAwaiter().GetResult();
+
+                    // Remove found display names, re-queue non-found
+                    if (res.IsSuccess)
+                    {
+                        // Add successful name fetches
+                        Dictionary<string, string> foundDisplayNames = new Dictionary<string, string>();
+                        List<string> notFoundDisplayNames = new List<string>();
+                        foreach (IdentityDisplayNamesResponse.Identity id in res.value.found)
+                        {
+                            // Set
+                            foundDisplayNames[id.uuid] = id.display_name;
+
+                            // Add to user manager
+                            if (!__instance._users._usersByUUID.ContainsKey(id.uuid))
+                            {
+                                // Add
+                                UserInfo uInfo = new UserInfo(id.uuid, id.display_name);
+                                __instance._users.Add(uInfo);
+                                __instance._users._usersByUUID[id.uuid] = uInfo;
+                            }
+                        }
+                        foreach (IdentityDisplayNamesResponse.Identity id in res.value.not_found)
+                        {
+                            // Add
+                            notFoundDisplayNames.Add(id.uuid);
+                        }
+
+                        // Done
+                        lock (inProgressDisplayNames)
+                        {
+                            foreach (string id in namesToFetch)
+                            {
+                                DisplayNameState promise = inProgressDisplayNames[id];
+                                if (foundDisplayNames.ContainsKey(id))
+                                    promise.Call(foundDisplayNames[id]);
+                                else
+                                {
+                                    Debug.LogError("Display name request failed: " + id + ", server did not recognize the name!");
+                                    promise.Error(new ArgumentException("ID not found: " + id));
+                                }
+                                inProgressDisplayNames.Remove(id);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Failure
+                        lock (inProgressDisplayNames)
+                        {
+                            foreach (string id in namesToFetch)
+                            {
+                                inProgressDisplayNames[id].Error(new ArgumentException("The server responded with an error, display name request failed for " + id + "!"));
+                                inProgressDisplayNames.Remove(id);
+                            }
+                        }
+                    }
+
+                    // Return
+                    return true;
+                });
             }
         }
     }
