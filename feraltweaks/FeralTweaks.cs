45a1ee43bbc4bcb4099d53a1d04344a4c6c26c3e
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2026-01-20 03:16:00 +0100

    Add the option to change voice server encryption...it was missing

--- feraltweaks/FeralTweaks.cs
+++ feraltweaks/FeralTweaks.cs
@@ -67,6 +67,7 @@ namespace feraltweaks
         public static int BlueboxPort = -1;
         public static int EncryptedGame = -1; // -1 = unset, 0 = false, 1 = true
         public static int EncryptedChat = -1; // -1 = unset, 0 = false, 1 = true
+        public static int EncryptedVoiceChat = -1; // -1 = unset, 0 = false, 1 = true
 
         // Error message container for when login fails and the server includes a feraltweaks message field in the response
         public static string LoginErrorMessage = null;
@@ -327,7 +328,7 @@ namespace feraltweaks
             // Parse environment
             string[] payload = args.Split(" ");
             if (payload.Length == 0)
-                LogError("Error: missing argument(s) for server environment: [directorhost] [apihost] [chathost] [chatport] [gameport] [voicehost] [voiceport] [blueboxport] [encryptedgame: true/false] [encryptedchat: true/false]");
+                LogError("Error: missing argument(s) for server environment: [directorhost] [apihost] [chathost] [chatport] [gameport] [voicehost] [voiceport] [blueboxport] [encryptedgame: true/false] [encryptedchat: true/false] [encryptedvoicechat: true/false]");
             try
             {
                 if (payload.Length >= 1)
@@ -379,6 +380,11 @@ namespace feraltweaks
                 {
                     EncryptedChat = payload[9].ToLower() == "true" ? 1 : 0;
                     LogInfo("Encryped chat server: " + (EncryptedChat == 1));
+                }
+                if (payload.Length >= 10)
+                {
+                    EncryptedVoiceChat = payload[10].ToLower() == "true" ? 1 : 0;
+                    LogInfo("Encryped voice chat server: " + (EncryptedVoiceChat == 1));
                 }
                 if (payload.Length >= 11)
                 {
