22e8055c77be6c646d83c29f7d452a752c140fda
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2026-01-20 14:10:38 +0100

    Correct serverenv

--- feraltweaks/FeralTweaks.cs
+++ feraltweaks/FeralTweaks.cs
@@ -344,42 +344,42 @@ namespace feraltweaks
                     EncryptedGame = payload[8].ToLower() == "true" ? 1 : 0;
                     LogInfo("Encryped game server: " + (EncryptedGame == 1));
                 }
-                if (payload.Length >= 9)
+                if (payload.Length >= 10)
                 {
                     EncryptedChat = payload[9].ToLower() == "true" ? 1 : 0;
                     LogInfo("Encryped chat server: " + (EncryptedChat == 1));
                 }
-                if (payload.Length >= 10)
+                if (payload.Length >= 11)
                 {
                     EncryptedVoiceChat = payload[10].ToLower() == "true" ? 1 : 0;
                     LogInfo("Encryped voice chat server: " + (EncryptedVoiceChat == 1));
                 }
-                if (payload.Length >= 11)
+                if (payload.Length >= 12)
                 {
                     PatchConfig["GameAssetsProd"] = payload[11];
                     LogInfo("Game Assets Prod: " + payload[11]);
                 }
-                if (payload.Length >= 12)
+                if (payload.Length >= 13)
                 {
                     PatchConfig["GameAssetsStage"] = payload[12];
                     LogInfo("Game Assets Stage: " + payload[12]);
                 }
-                if (payload.Length >= 13)
+                if (payload.Length >= 14)
                 {
                     PatchConfig["GameAssetsDev"] = payload[13];
                     LogInfo("Game Assets Dev: " + payload[13]);
                 }
-                if (payload.Length >= 14)
+                if (payload.Length >= 15)
                 {
                     PatchConfig["GameAssetsShared"] = payload[14];
                     LogInfo("Game Assets Shared Prod: " + payload[14]);
                 }
-                if (payload.Length >= 15)
+                if (payload.Length >= 16)
                 {
                     PatchConfig["GameAssetsStageShared"] = payload[15];
                     LogInfo("Game Assets Shared Stage: " + payload[15]);
                 }
-                if (payload.Length >= 16)
+                if (payload.Length >= 17)
                 {
                     PatchConfig["GameAssetsDevShared"] = payload[16];
                     LogInfo("Game Assets Shared Dev: " + payload[16]);
