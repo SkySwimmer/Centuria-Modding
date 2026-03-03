55a36e5a8e9ec854dc2de49c9e837874949f210b
Sky Swimmer <stefaneekhof@gmail.com>
2026-03-03 15:30:52 +0100

    Update launcher and installer

--- launcher/windows-self-extracting-installer/CenturiaSelfExtractingInstaller/Program.cs
+++ launcher/windows-self-extracting-installer/CenturiaSelfExtractingInstaller/Program.cs
@@ -19,7 +19,6 @@ namespace CenturiaSelfExtractingInstaller
         [STAThread]
         static void Main(string[] args)
         {
-            bool hasInstallerData = true;
             foreach (string arg in args)
             {
                 if (Arguments != "")
@@ -28,80 +27,6 @@ namespace CenturiaSelfExtractingInstaller
                     Arguments += "\"" + arg.Replace("\\\"", "\\\\\"").Replace("\"", "\\\"");
                 else
                     Arguments += arg;
-            }
-
-            // Check for installer
-            string exe = Application.ExecutablePath;
-            Stream strm = File.OpenRead(exe);
-
-            // Seek to end
-            byte[] magic = Encoding.UTF8.GetBytes("CENTURIA!LAUNCHER");
-            strm.Position = strm.Length - magic.Length;
-
-            // Read
-            byte[] current = new byte[magic.Length];
-            int read = strm.Read(current, 0, current.Length);
-
-            // Verify
-            for (int i = 0; i < current.Length; i++)
-                if (current[i] != magic[i])
-                {
-                    hasInstallerData = false;
-                    break;
-                }
-
-            // Check arguments
-            if (args.Length >= 1 && !hasInstallerData)
-            {
-                if (!File.Exists(args[0]))
-                {
-                    MessageBox.Show("Invalid argument: expected a zip file", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    Environment.Exit(1);
-                }
-
-                Stream str = File.OpenRead(args[0]);
-                try
-                {
-                    ZipArchive a = new ZipArchive(str);
-                    a.Entries.ToArray();
-                    a.Dispose();
-                    str = File.OpenRead(args[0]);
-                }
-                catch
-                {
-                    MessageBox.Show("Invalid argument: expected a zip file", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    Environment.Exit(1);
-                }
-
-                // Copy data
-                Console.WriteLine("Copying zip into installer...");
-                FileStream o = File.OpenWrite(args.Length == 1 ? "installer.exe" : args[1]);
-                strm.Position = 0;
-                strm.CopyTo(o);
-
-                // Write data
-                long pos = o.Position;
-                str.CopyTo(o);
-                str.Close();
-
-                // Write headers
-                byte[] h = BitConverter.GetBytes(pos);
-                if (!BitConverter.IsLittleEndian)
-                    Array.Reverse(h);
-                o.Write(h, 0, h.Length);
-                o.Write(magic, 0, magic.Length);
-
-                // Close output
-                o.Close();
-                Environment.Exit(0);
-            }
-
-            // Close stream
-            strm.Close();
-            if (!hasInstallerData)
-            { 
-                MessageBox.Show("No data present in this installer!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(1);
             }
 
             // Start app
